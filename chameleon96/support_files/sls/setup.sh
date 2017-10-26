0ea5a1e3507fdc1a8d877cbb8b78bd5fbef21f51
engineer_novtech <engineer_novtech@26bed028-1544-48b3-99ad-e59f3b87ba3e>
2017-10-26 01:30:23 +0000

    cleanup for release

--- chameleon96/support_files/sls/setup.sh
+++ chameleon96/support_files/sls/setup.sh
@@ -2,7 +2,10 @@
 
 LINUX_PATH='../../chameleon96/build/linux-custom'
 
+mkdir -p ${LINUX_PATH}/include/config/mmc/sdhci/of/sls
 cp sdhc.h ${LINUX_PATH}/include/config/mmc/sdhci/of/sls/sdhc.h
+
+mkdir -p ${LINUX_PATH}/drivers/mmc/host/
 
 cp sdhci-of-slssdhc.o ${LINUX_PATH}/drivers/mmc/host/sdhci-of-slssdhc.o
 cp Makefile	${LINUX_PATH}/drivers/mmc/host/Makefile
